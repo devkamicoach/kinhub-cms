64b2e84f4c10ab16efdab0d597f953feb893cd19
RamonMamon <ramoncatane@gmail.com>
2023-03-21 21:26:34 +0800

    build: updated security permissions

--- config/database.ts
+++ config/database.ts
@@ -45,7 +45,7 @@ export default ({ env }) => {
           cipher: env("DATABASE_SSL_CIPHER", undefined),
           rejectUnauthorized: env.bool(
             "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
+            false
           ),
         },
         schema: env("DATABASE_SCHEMA", "public"),
@@ -53,7 +53,6 @@ export default ({ env }) => {
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
         max: env.int("DATABASE_POOL_MAX", 10),
-        acquireTimeoutMillis: 300000,
       },
     },
     sqlite: {
