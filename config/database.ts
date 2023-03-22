4e115f9abb9ba09b3d47af296d71055374ea86a9
RamonMamon <ramoncatane@gmail.com>
2023-03-22 22:04:31 +0800

    build: replace newline stuff

--- config/database.ts
+++ config/database.ts
@@ -3,6 +3,8 @@ import path from "path";
 export default ({ env }) => {
   const client = env("DATABASE_CLIENT", "sqlite");
 
+  const ca = env("DATABASE_SSL_CA", undefined).replace(/\\n/g, "\n");
+
   const connections = {
     mysql: {
       connection: {
@@ -15,7 +17,7 @@ export default ({ env }) => {
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
+          ca: ca,
           capath: env("DATABASE_SSL_CAPATH", undefined),
           cipher: env("DATABASE_SSL_CIPHER", undefined),
           rejectUnauthorized: env.bool(
@@ -40,7 +42,7 @@ export default ({ env }) => {
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
+          ca,
           capath: env("DATABASE_SSL_CAPATH", undefined),
           cipher: env("DATABASE_SSL_CIPHER", undefined),
           rejectUnauthorized: env.bool(
